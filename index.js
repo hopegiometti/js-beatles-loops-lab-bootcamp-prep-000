f326dcd08b377a306d802cd5878301a8f016c801
Hope Giometti <hopegiometti@gmail.com>
2019-08-08 21:50:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ function theBeatlesPlay(players, instruments) {
 }
 
 
-var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
+
 
 function johnLennonFacts(facts) {
   var newFacts = [];
